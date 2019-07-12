48c832624441b7a5b539b01a716a27b48f1ae01b
Acivii <Acivii0312@163.com>
2019-07-12 23:49:42 +0800

    加入 parcel 并将button组件改为单文件组件

---
+++ src/button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button class="g-button">快点我</button>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss">
+  .g-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    margin: 5em;
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    &:hover {
+      border-color: var(--border-color-hover);
+    }
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+</style> 
