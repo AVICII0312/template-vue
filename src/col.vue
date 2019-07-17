8a110d611c5a9e57afdb857cdd022ef7c1b582f5
Acivii <Acivii0312@163.com>
2019-07-17 11:43:34 +0800

    重构 row 和 col

--- src/col.vue
+++ src/col.vue
@@ -1,53 +1,54 @@
 <template>
-  <div class="col" :class="[`col-${span}`, offset && `offset-${offset}`]"
-    :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}"
-  >
-  <div style="border:1px solid green; height:100px;">
+  <div class="col" :class="colClass" :style="colStyle">
     <slot></slot>
-  </div>
   </div>
 </template>
 <script>
 export default {
-    name:'templateCol',
-    props:{
-        span:{
-            type:[Number,String]
-        },
-        offset:{
-            type:[Number,String]
-        }
+  name: "templateCol",
+  props: {
+    span: {
+      type: [Number, String]
     },
-    data(){
-        return{
-            gutter:0
-        }
-    },
-    created (){
-        console.log('col created')
-    },
-    mounted (){
-        console.log('col mounted')
+    offset: {
+      type: [Number, String]
     }
-    
-}
+  },
+  data() {
+    return {
+      gutter: 0
+    };
+  },
+  computed: {
+    colClass() {
+      let { span, offset } = this;
+      return [span && `col-${span}`, offset && `offset-${offset}`];
+    },
+    colStyle() {
+      return {
+        paddingLeft: this.gutter / 2 + "px",
+        paddingRight: this.gutter / 2 + "px"
+      };
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-    .col {
-        height: 100px;
-        width: 50%;
-        $class-prefix: col-;
-        @for $n from 1 through 24 {
-            &.#{$class-prefix}#{$n}{
-                width:($n / 24) *100% ; 
-            }
-        }
-        $class-prefix: offset-;
-        @for $n from 1 through 24 {
-            &.#{$class-prefix}#{$n}{
-                margin-left:($n / 24) *100% ; 
-            }
-        }
+.col {
+  height: 100px;
+  width: 50%;
+  $class-prefix: col-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: ($n / 24) * 100%;
     }
+  }
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n / 24) * 100%;
+    }
+  }
+}
 </style>
