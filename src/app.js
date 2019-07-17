c51135ef2485547d3f65506704e8082d9a8bfa9d
Acivii <Acivii0312@163.com>
2019-07-17 23:27:02 +0800

    创建 layout content footer sider 组件

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import Buttongroup from './button-group'
 import Input from './input'
 import Row from './row'
 import Col from './col'
+import Layout from './layout'
+import Header from './header'
+import Footer from './footer'
+import Content from './content'
+import Sider from './sider'
 
 Vue.component('g-button-group', Buttongroup)
 Vue.component('g-button', Button)
@@ -12,6 +17,13 @@ Vue.component('g-icon', Icon)
 Vue.component('g-input',Input)
 Vue.component('g-row',Row)
 Vue.component('g-col',Col)
+Vue.component('g-layout',Layout)
+Vue.component('g-header',Header)
+Vue.component('g-content',Content)
+Vue.component('g-footer',Footer)
+Vue.component('g-sider',Sider)
+
+
 new Vue({
   el: '#app',
   data: {
