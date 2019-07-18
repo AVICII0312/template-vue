62f8c428e3364f1d0add5c59813c1e45a148cfb6
Acivii <Acivii0312@163.com>
2019-07-18 15:51:16 +0800

    基本完成组件网站

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 import Button from './src/button'
 import Buttongroup from './src/button-group'
 import Icon from './src/icon'
+import Input from './src/input'
+import Row from './src/row'
+import Col from './src/col'
+import Layout from './src/layout'
+import Header from './src/header'
+import Content from './src/content'
+import Sider from './src/sider'
+import Footer from './src/footer'
 
-export {Button,Buttongroup,Icon}
+export {
+    Button,
+    Buttongroup,
+    Input,
+    Icon,
+    Col,
+    Row,
+    Layout,
+    Header,
+    Content,
+    Sider,
+    Footer,
+}
