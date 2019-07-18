f2930b529dc39924264add6745570c1f7a98d169
Acivii <Acivii0312@163.com>
2019-07-18 11:16:30 +0800

    更改button样式

--- src/button.vue
+++ src/button.vue
@@ -1,72 +1,93 @@
 <template>
-  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}"
-   @click="$emit('click')">
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click')">
     <g-icon v-if="icon && !loading" :name="icon" class="icon"></g-icon>
     <g-icon name="loading" class="loading icon" v-if="loading"></g-icon>
     <div class="content">
-    <slot></slot></div>
-    
+      <slot></slot>
+    </div>
   </button>
 </template>
 <script>
-import Icon from './icon'
+import Icon from "./icon";
 export default {
-  components:{
-    'g-icon':Icon
+  name:'templateButton',
+  components: {
+    "g-icon": Icon
   },
-  props:{
+  props: {
     icon: {},
-    loading:{
-      type:Boolean,
-      default:false
+    loading: {
+      type: Boolean,
+      default: false
     },
-    iconPosition:{
-      type:String,
-      default:'left',
-      validator(value){
-        return !( value !=='left' && value !=="right")
+    iconPosition: {
+      type: String,
+      default: "left",
+      validator(value) {
+        return !(value !== "left" && value !== "right");
       }
     },
-    methods:{
-    }
+    methods: {}
   },
-  name:'template-button'
-}
+  name: "template-button"
+};
 </script>
 <style lang="scss" scoped>
-  .g-button {
-    font-size: var(--font-size);
-    height: var(--button-height);
-    padding: 0 1.1em;
-    border-radius: var(--border-radius);
-    border: 1px solid var(--border-color);
-    background: var(--button-bg);
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    vertical-align: middle;
-    &:hover {
-      border-color: var(--border-color-hover);
-    }
-    &:active {
-      background-color: var(--button-active-bg);
-    }
-    &:focus {
-      outline: none;
-    }
-    > .icon{order: 1; margin-right: .3em;}
-    > .content{order: 2;}
-    &.icon-right{
-      > .icon{order: 2; margin-left: .3em;margin-right: 0;}
-      > .content{order: 1;}
-    }
-    @keyframes spin {
-      0%{ transform: rotate(0deg);}
-      100%{transform: rotate(360deg);}
-    }
-    .loading{
-      animation: spin 1s infinite linear;
-    }
-   
+$button-height: 32px;
+$font-size: 14px;
+$button-bg: white;
+$button-active-bg: #eee;
+$border-radius: 4px;
+$color: #333;
+$border-color: #999;
+$border-color-hover: #666;
+.g-button {
+  font-size: $font-size;
+  height: $button-height;
+  padding: 0 1.1em;
+  border-radius: $border-radius;
+  border: 1px solid $border-color;
+  background: $button-bg;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  &:hover {
+    border-color: $border-color-hover;
   }
+  &:active {
+    background-color: $button-active-bg;
+  }
+  &:focus {
+    outline: none;
+  }
+  > .icon {
+    order: 1;
+    margin-right: 0.3em;
+  }
+  > .content {
+    order: 2;
+  }
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin-left: 0.3em;
+      margin-right: 0;
+    }
+    > .content {
+      order: 1;
+    }
+  }
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+  .loading {
+    animation: spin 1s infinite linear;
+  }
+}
 </style> 
